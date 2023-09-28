dfe157146ce53bc68383e3f0e99ab5478adbef1c
udsoncosta <udsoncostasantana@gmail.com>
2023-09-28 16:41:22 -0300

    Atualizado

--- Service/Implements/ProdutoService.cs
+++ Service/Implements/ProdutoService.cs
@@ -49,12 +49,12 @@ namespace LojaGames.Service.Implements
 
             if (produto.Categoria is not null)
             {
-                var BuscaTema = await _context.Categorias.FindAsync(produto.Categoria.Id);
+                var BuscaCategoria = await _context.Categorias.FindAsync(produto.Categoria.Id);
 
-                if (BuscaTema is null)
+                if (BuscaCategoria is null)
                     return null;
 
-                produto.Categoria = BuscaTema;
+                produto.Categoria = BuscaCategoria;
 
             }
             await _context.Produtos.AddAsync(produto);
